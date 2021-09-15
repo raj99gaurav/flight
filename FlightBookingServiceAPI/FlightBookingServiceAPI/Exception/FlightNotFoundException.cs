130406d33543512beec56c2b06ff9dff26c31593
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2021-09-15 07:58:44 +0530

    Add files via upload

---
+++ FlightBookingServiceAPI/FlightBookingServiceAPI/Exception/FlightNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightBookingServiceAPI.Exception
+{
+    public class FlightNotFoundException
+    {
+    }
+}
